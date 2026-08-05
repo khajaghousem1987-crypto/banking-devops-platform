5037f4d3c901585945f015e3f3b79afeccec0d11
khajaghousem1987-crypto <khajaghousem1987@gmail.com>
2026-08-05 10:22:24 +0530

    Updated OPA Scripts

--- scripts/opa-check.sh
+++ scripts/opa-check.sh
@@ -8,30 +8,48 @@ echo "========================================="
  
 echo ""
 echo "Checking Security Group Policies..."
-opa eval \
-  --fail-defined \
-  --format=pretty \
-  -d policies \
-  -i iac/tfplan.json \
-  "data.terraform.security.deny"
+ 
+SG=$(opa eval --format=raw \
+-d policies \
+-i iac/tfplan.json \
+"data.terraform.security.deny")
+ 
+echo "$SG"
+ 
+if [ "$SG" != "[]" ]; then
+  echo "Security Group Policy Failed"
+  exit 1
+fi
  
 echo ""
 echo "Checking Mandatory Tag Policies..."
-opa eval \
-  --fail-defined \
-  --format=pretty \
-  -d policies \
-  -i iac/tfplan.json \
-  "data.terraform.tags.deny"
+ 
+TAG=$(opa eval --format=raw \
+-d policies \
+-i iac/tfplan.json \
+"data.terraform.tags.deny")
+ 
+echo "$TAG"
+ 
+if [ "$TAG" != "[]" ]; then
+  echo "Mandatory Tags Policy Failed"
+  exit 1
+fi
  
 echo ""
 echo "Checking S3 Policies..."
-opa eval \
-  --fail-defined \
-  --format=pretty \
-  -d policies \
-  -i iac/tfplan.json \
-  "data.terraform.s3.deny"
+ 
+S3=$(opa eval --format=raw \
+-d policies \
+-i iac/tfplan.json \
+"data.terraform.s3.deny")
+ 
+echo "$S3"
+ 
+if [ "$S3" != "[]" ]; then
+  echo "S3 Policy Failed"
+  exit 1
+fi
  
 echo ""
 echo "========================================="
