f2c56165e70a8853ebe2ab7a9840dd81ee95d7f2
khajaghousem1987-crypto <khajaghousem1987@gmail.com>
2026-08-11 09:58:31 +0530

    Fixed Outbound rule

--- iac/modules/security-group/main.tf
+++ iac/modules/security-group/main.tf
@@ -93,7 +93,7 @@ resource "aws_vpc_security_group_egress_rule" "all" {
 
   security_group_id = aws_security_group.banking_app_sg.id
 
-  cidr_ipv4 = "49.204.5.136/32"
+  cidr_ipv4 = "0.0.0.0/0"
 
   ip_protocol = "-1"
 
